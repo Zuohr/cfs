3cec6e8bed1d4bd8aa403e85b9ce63a883a075af
user <user@10.0.0.8>
2014-01-21 08:38:51 -0500

    changepwForm

---
+++ cfs/src/pentagon/cfs/formbean/changepwForm.java
@@ -0,0 +1,42 @@
+package pentagon.cfs.formbean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class changepwForm {
+	private String confirmPassword;
+	private String newPassword;
+	
+	public String getConfirmPassword() { return confirmPassword; }
+	public String getNewPassword()     { return newPassword;     }
+	
+	public void setConfirmPassword(String s) { confirmPassword = s.trim(); }
+	public void setNewPassword(String s)     { newPassword     = s.trim(); }
+
+	public List<String> getValidationErrors() {
+		List<String> errors = new ArrayList<String>();
+
+		if (newPassword == null || newPassword.length() == 0) {
+			errors.add("New Password is required");
+		}
+		
+		if (confirmPassword == null || confirmPassword.length() == 0) {
+			errors.add("Confirm Pwd is required");
+		}
+		
+		if (errors.size() > 0) {
+			return errors;
+		}
+		
+		if (!newPassword.equals(confirmPassword)) {
+			errors.add("Passwords do not match");
+		}
+
+		return errors;
+	}
+}
+
+
+
+
