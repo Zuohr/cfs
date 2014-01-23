726c25baa1f4f6b854b8e553a15d60e1172b85b4
Zuohr <haoran.zuo@gmail.com>
2014-01-22 20:30:51 -0500

    modify CreateFundForm

--- cfs/src/pentagon/cfs/formbean/CreateFundForm.java
+++ cfs/src/pentagon/cfs/formbean/CreateFundForm.java
@@ -4,49 +4,49 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 
+import pentagon.cfs.databean.Fund;
+
 public class CreateFundForm {
 	private String fundName;
 	private String fundTicker;
 	private boolean complete = true;
 	private ArrayList<String> errors;
-	
-	public CreateFundForm(HttpServletRequest request){
-		this.fundName = (String) request.getParameter("fundName");
-		this.fundTicker = (String) request.getParameter("fundTicker");
+
+	public CreateFundForm(HttpServletRequest request) {
+		this.fundName = (String) request.getParameter("fundname");
+		this.fundTicker = (String) request.getParameter("ticker");
 		int size = 2;
 		this.errors = new ArrayList<String>(size);
-		for(int i=0; i<size; i++){
+		for (int i = 0; i < size; i++) {
 			errors.add("");
 		}
 		checkErrors();
 	}
 
-	public ArrayList<String> getErrors(){
+	public Fund getFund() {
+		Fund fund = new Fund();
+		fund.setName(fundName);
+		fund.setSymbol(fundTicker);
+		return fund;
+	}
+
+	public ArrayList<String> getErrors() {
 		return errors;
 	}
-	
-	public boolean isComplete(){
+
+	public boolean isComplete() {
 		return complete;
 	}
-	
-	public String getFundName(){
-		return fundName;
-	}
-	public void setFundName(String name){
-		fundName = name;
-	}
-	public void getFundTicker(String ticker){
-		fundTicker = ticker;
-	}
-	private void checkErrors(){
-		if(fundName == null||fundName.trim().isEmpty()){
+
+	private void checkErrors() {
+		if (fundName == null || fundName.trim().isEmpty()) {
 			errors.set(0, "Fund name cannot be empty.");
 			complete = false;
 		}
-		if(fundTicker==null||fundTicker.trim().isEmpty()){
-			errors.set(0, "Fund ticker cannot be empty");
+		if (fundTicker == null || fundTicker.trim().isEmpty()) {
+			errors.set(1, "Fund ticker cannot be empty");
 			complete = false;
 		}
 	}
-	
+
 }
