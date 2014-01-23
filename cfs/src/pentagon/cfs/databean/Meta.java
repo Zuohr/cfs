4e01e7e1ac7ee3d0eda5a65fc51b6adce7e68b72
Zuohr <haoran.zuo@gmail.com>
2014-01-23 10:44:02 -0500

    add meta

--- cfs/src/pentagon/cfs/databean/Meta.java
+++ cfs/src/pentagon/cfs/databean/Meta.java
@@ -3,11 +3,23 @@ package pentagon.cfs.databean;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.genericdao.PrimaryKey;
+
+@PrimaryKey("id")
 public final class Meta {
 	public static final String DATE_FORMAT = "MM/dd/yyyy";
 	public static String lastDate = new SimpleDateFormat(DATE_FORMAT)
 			.format(new Date());
 	private String date;
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public String getDate() {
 		return date;
