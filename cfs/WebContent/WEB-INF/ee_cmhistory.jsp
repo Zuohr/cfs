68c2da2541443b9d1142af776a08c6920c65ef8b
微尘 <@128.237.221.174>
2014-01-23 20:27:02 -0500

    [weicheng zhu] view ee_cmhistory

---
+++ cfs/WebContent/WEB-INF/ee_cmhistory.jsp
@@ -0,0 +1,59 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+
+
+<jsp:include page="html_header.jsp" />
+<body>
+	<div id="wrap">
+		<jsp:include page="header.jsp" />
+
+		<div class="container">
+			<jsp:include page="cm_nav.jsp" />
+			<div class="col-xs-12 col-sm-9">
+				<h2>Transaction History</h2>
+                <h4>User ID:${requestScope.id }</h4>
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th class="header-title">Date</th>
+							<th class="header-date">Operation</th>
+							<th class="header-ac">Fund Name</th>
+							<th class="header-ac">Number</th>
+							<th class="header-ac">Price</th>
+							<th class="header-ac">Amount</th>
+							<th class="header-ac">Status</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						<c:forEach var="record" items="${requestScope.records }">
+							<tr>
+								<td>${record.date }</td>
+								<td>${record.type }</td>
+								<td>${record.fundname }</td>
+								<%-- <td><a href="#">${record.fundname }</a></td> --%>
+								<td>${record.share }</td>
+								<td>${record.price }</td>
+								<td>${record.dollar }</td>
+								<td>${record.state }</td>
+							</tr>
+						</c:forEach>
+					</tbody>
+				</table>
+
+			</div>
+			<!--/span-->
+
+
+		</div>
+		<!--/row-->
+
+
+	</div>
+	<!--/.container-->
+
+	<jsp:include page="footer.jsp" />
+</body>
+</html>
--- cfs/src/pentagon/cfs/action/EmplViewTranHistroy.java
+++ cfs/src/pentagon/cfs/action/EmplViewTranHistroy.java
@@ -1,8 +1,6 @@
 //wc
 package pentagon.cfs.action;
 
-
-
 import java.text.SimpleDateFormat;
 import javax.servlet.http.HttpServletRequest;
 import org.genericdao.RollbackException;
@@ -19,7 +17,7 @@ public class EmplViewTranHistroy implements Action {
 	private Model model;
 
 	public EmplViewTranHistroy(Model model) {
-		
+
 		this.model = model;
 	}
 
@@ -31,11 +29,15 @@ public class EmplViewTranHistroy implements Action {
 
 	@Override
 	public String perform(HttpServletRequest request) throws RollbackException {
-		Employee employee=(Employee) request.getSession().getAttribute("Employee"); 
+
+		Employee employee = (Employee) request.getSession().getAttribute(
+				"employee");
+
 		if (employee == null) {
 			return "login.do";
 		} else {
 			int cm_id = Integer.parseInt(request.getParameter("id"));
+			request.setAttribute("id", request.getParameter("id"));
 			TransactionDAO dao = model.getTransactionDAO();
 			TransactionRecord[] transactions = dao.getHistory(cm_id);
 			Record[] records = new Record[transactions.length];
@@ -43,10 +45,11 @@ public class EmplViewTranHistroy implements Action {
 				records[i] = new Record(transactions[i]);
 			}
 			request.setAttribute("records", records);
-			return "cm_history.jsp";
-		
+			return "ee_cmhistory.jsp";
+
+		}
 	}
-}
+
 	public class Record {
 		private String date;
 		private String type;
@@ -56,62 +59,64 @@ public class EmplViewTranHistroy implements Action {
 		private String dollar;
 		private String state;
 		private String url;
-	public Record(TransactionRecord rd) {
-		SimpleDateFormat sdf = new SimpleDateFormat(Meta.DATE_FORMAT);
-		this.date = sdf.format(rd.getDate());
-		this.type = rd.getType();
-		FundDAO fundDAO = model.getFundDAO();
-		try {
-			if (rd.getFund_id() != 0) {
-				Fund fund = fundDAO.read(Integer.valueOf(rd.getFund_id()));
-				this.fundname = String.format(
-						"<a href=\"researchfund.do?fund_id=%s\">%s(%s)</a>",
-						String.valueOf(rd.getFund_id()), fund.getName(), fund.getSymbol());
-				this.share = String.valueOf(100);
-				this.price = String.valueOf(0.1);
-			} else {
-				this.fundname = "-";
-				this.share = "-";
-				this.price = "-";
+
+		public Record(TransactionRecord rd) {
+			SimpleDateFormat sdf = new SimpleDateFormat(Meta.DATE_FORMAT);
+			this.date = sdf.format(rd.getDate());
+			this.type = rd.getType();
+			FundDAO fundDAO = model.getFundDAO();
+			try {
+				if (rd.getFund_id() != 0) {
+					Fund fund = fundDAO.read(Integer.valueOf(rd.getFund_id()));
+					this.fundname = String
+							.format("<a href=\"researchfund.do?fund_id=%s\">%s(%s)</a>",
+									String.valueOf(rd.getFund_id()),
+									fund.getName(), fund.getSymbol());
+					this.share = String.valueOf(100);
+					this.price = String.valueOf(0.1);
+				} else {
+					this.fundname = "-";
+					this.share = "-";
+					this.price = "-";
+				}
+			} catch (RollbackException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (RollbackException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			this.dollar = String.valueOf(50);
+			this.state = rd.isComplete() ? "completed" : "pending";
 		}
-		this.dollar = String.valueOf(50);
-		this.state = rd.isComplete() ? "completed" : "pending";
-	}
 
-	public String getDate() {
-		return date;
-	}
+		public String getDate() {
+			return date;
+		}
 
-	public String getType() {
-		return type;
-	}
+		public String getType() {
+			return type;
+		}
 
-	public String getFundname() {
-		return fundname;
-	}
+		public String getFundname() {
+			return fundname;
+		}
 
-	public String getShare() {
-		return share;
-	}
+		public String getShare() {
+			return share;
+		}
 
-	public String getPrice() {
-		return price;
-	}
+		public String getPrice() {
+			return price;
+		}
 
-	public String getDollar() {
-		return dollar;
-	}
+		public String getDollar() {
+			return dollar;
+		}
 
-	public String getState() {
-		return state;
-	}
+		public String getState() {
+			return state;
+		}
 
-	public String getUrl() {
-		return url;
+		public String getUrl() {
+			return url;
+		}
 	}
-}
 }
