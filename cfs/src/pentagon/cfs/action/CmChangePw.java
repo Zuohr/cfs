d2287b40b6674cd0041db238ff756b67cff8bb44
user <user@128.237.249.218>
2014-01-23 23:39:44 -0500

    CmChangePw

--- cfs/src/pentagon/cfs/action/CmChangePw.java
+++ cfs/src/pentagon/cfs/action/CmChangePw.java
@@ -2,24 +2,17 @@ package pentagon.cfs.action;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 import pentagon.cfs.model.Model;
 import pentagon.cfs.dao.CustomerDAO;
-
 import org.genericdao.RollbackException;
-
 import pentagon.cfs.databean.Customer;
 import pentagon.cfs.formbean.ChangepwForm;
-import pentagon.cfs.dao.FundDAO;
-import pentagon.cfs.databean.Employee;
-import pentagon.cfs.databean.Fund;
-import pentagon.cfs.formbean.CreateFundForm;
 
 
 public class CmChangePw implements Action {
+
 	private Model model;
 
 	public CmChangePw(Model model) {
@@ -32,30 +25,41 @@ public class CmChangePw implements Action {
 		HttpSession session = request.getSession();
 		Customer customer = (Customer) session.getAttribute("customer");
 		ChangepwForm form = new ChangepwForm(request);
-		
+
 		if (customer == null) {
 			return "login.do";
-		}if ("submit".equals(request.getParameter("cmchangepw_btn"))){
-			if (!customer.getPassword().equals(form.getoldPassword())){
+		}else{
+		if ("submit".equals(request.getParameter("cmchangepw_btn"))) {
+			if (!customer.getPassword().equals(form.getoldPassword())) {
 				errors.add("Old Password is Not Correct!");
-			}
+				return "cm_changepw.jsp";
+			}else{
 			String newPassword = form.getnewPassword();
 			customer.setPassword(newPassword);
 			try {
 				// Change the password
-				CustomerDAO.updateCustomer(customer);
-				
-				request.setAttribute("errors", "Password changed for "
-						+ customer.getFirstname() + " " + customer.getLastname());
+				CustomerDAO cmDAO = model.getCustomerDAO();
+				cmDAO.updateCustomer(customer);
+
+				request.setAttribute("errors",
+						"Password changed for " + customer.getFirstname() + " "
+								+ customer.getLastname());
 				return "cm_changepw.jsp";
 			} catch (RollbackException e) {
 				errors.add(e.toString());
 				return "cm_changepw.jsp";
 			}
-				
-			
+			}
 		}
+		}
+		return "cm_changepw.jsp";
 
-}
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
